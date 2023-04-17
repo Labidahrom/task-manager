bfcb84d766c212c2b54f9b3acd21b3dcbe0d6466
Labidahrom <labidahrom@yandex.ru>
2023-04-17 12:24:57 +0300

    check

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 DATABASE_URL = os.getenv("DATABASE_URL")
-DJANGO_DEBUG = os.getenv("DJANGO_DEBUG")
+# DJANGO_DEBUG = os.getenv("DJANGO_DEBUG")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DJANGO_DEBUG', '1')))
