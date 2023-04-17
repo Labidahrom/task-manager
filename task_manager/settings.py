1317357b3400535d14c74f3c413f1af50489afb7
Labidahrom <labidahrom@yandex.ru>
2023-04-17 11:02:20 +0300

    some changes

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-RAILWAY_POSTGRESQL_URL = os.getenv("RAILWAY_POSTGRESQL_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,9 +87,9 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-if 'RAILWAY_POSTGRESQL_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.config(default=os.environ.get('RAILWAY_POSTGRESQL_URL'), engine='django.db.backends.postgresql')
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), engine='django.db.backends.postgresql')
     }
 else:
     DATABASES = {
