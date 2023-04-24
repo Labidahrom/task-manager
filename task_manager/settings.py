8009c7dfb670116538452745958ff77f1b3e452f
Labidahrom <labidahrom@yandex.ru>
2023-04-24 17:32:38 +0300

    fix language

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -114,7 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGES = [('en', 'English'), ('ru', 'Russian')]
+
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
