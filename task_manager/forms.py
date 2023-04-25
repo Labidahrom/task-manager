fbaae44b688c890ebf3e97007beaa94e9b9d2921
Labidahrom <labidahrom@yandex.ru>
2023-04-25 09:01:35 +0300

    fix

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -6,6 +6,7 @@ import django_filters
 class BootstrapMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.label_suffix = ""
         for field in self.fields:
             self.fields[field].widget.\
                 attrs.update({'class': 'form-control mb-3'})
