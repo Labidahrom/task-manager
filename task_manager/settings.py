c2373e0d493a373a30582d48d90c5054722df0bd
Labidahrom <labidahrom@yandex.ru>
2023-05-03 08:07:38 +0600

    fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
