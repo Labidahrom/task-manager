acf23a6d10ec7e6ad9cdf54cd11be8e169be92fc
Labidahrom <labidahrom@yandex.ru>
2023-05-13 18:05:53 +0600

    fix

--- task_manager/views.py
+++ task_manager/views.py
@@ -70,7 +70,7 @@ class UpdateUser(View):
             user = form.save(commit=False)
             user.set_password(request.POST.get('password1'))
             user.save()
-            messages.success(request, 'Пользователь успешно изменен')
+            messages.success(request, 'Пользователь успешно изменён')
             return redirect(reverse('users_list'))
         messages.warning(request, 'ошибка')
         return render(request, 'update_user.html', {'form': form})
