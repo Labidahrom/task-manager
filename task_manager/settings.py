7962e46580200d29cd29f184eb5bc8f97a2dd880
Labidahrom <labidahrom@yandex.ru>
2023-05-13 09:50:40 +0600

    fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -15,11 +15,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 DATABASE_URL = os.getenv("DATABASE_URL")
-DEBUG = os.getenv('DJANGO_DEBUG', 'True')
+DJANGO_DEBUG = os.getenv("DJANGO_DEBUG")
 POST_SERVER_ITEM_ACCESS_TOKEN = os.getenv("POST_SERVER_ITEM_ACCESS_TOKEN")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
+DEBUG = bool(int(os.environ.get('DJANGO_DEBUG', '1')))
 
 ALLOWED_HOSTS = [
     '*',
@@ -78,7 +79,7 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-if DEBUG:
+if not DEBUG:
     DATABASES = {
         'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), engine='django.db.backends.postgresql')
     }
