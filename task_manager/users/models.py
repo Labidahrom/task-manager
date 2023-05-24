76f74124a665f549c2e16c7d2397795c81c2105c
Labidahrom <labidahrom@yandex.ru>
2023-05-24 12:48:10 +0600

    fix

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,25 +1,7 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
-from django.utils import timezone
-
-
-# class UserManager(BaseUserManager):
-#
-#     def create_superuser(self, username, password, first_name=None,
-#                          last_name=None, **extra_fields):
-#         extra_fields['is_stuff'] = True
-#         return self.create_user(username, password, first_name,
-#                                 last_name, **extra_fields)
-#
-#     def get_by_natural_key(self, username):
-#         return self.get(**{self.model.USERNAME_FIELD: username})
-#
-#     def _is_superuser(self, extra_fields):
-#         return extra_fields.get('is_stuff')
+from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    # objects = UserManager()
 
     def __str__(self):
         return self.get_full_name()
